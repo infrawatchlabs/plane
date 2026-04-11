ac68ae019a18758ad0605d4c14bdf04cbead8f4f
Surya <surya@infrawatchlabs.com>
2026-04-11 14:44:00 +0530

    fix(wiki): Enable CMD+K command palette in Wiki layout
    PowerKProvider was only mounted in the Projects layout. Add it to
    the Wiki layout so CMD+K works when viewing wiki pages.

--- apps/web/app/(all)/[workspaceSlug]/(wiki)/layout.tsx
+++ apps/web/app/(all)/[workspaceSlug]/(wiki)/layout.tsx
@@ -6,12 +6,15 @@
 
 import { observer } from "mobx-react";
 import { Outlet } from "react-router";
+// components
+import { ProjectsAppPowerKProvider } from "@/components/power-k/projects-app-provider";
 // local imports
 import { WikiAppSidebar } from "./wiki-sidebar";
 
 function WikiLayout() {
   return (
     <div className="relative flex h-full w-full flex-col overflow-hidden rounded-lg border border-subtle">
+      <ProjectsAppPowerKProvider />
       <div className="relative flex size-full overflow-hidden">
         <WikiAppSidebar />
         <main className="relative flex h-full w-full flex-col overflow-hidden bg-surface-1">
